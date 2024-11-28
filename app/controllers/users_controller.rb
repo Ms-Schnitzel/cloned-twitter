daaeafdf72603f7e3164877e53a6dbf72e946ac9
Spartan N7 <zachnielsen72@gmail.com>
2024-11-27 20:25:29 -0700

    first submit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+  def create
+    # @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.create(user_params)
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+
+    # render 'users/create.jbuilder'
+    if @user.save
+      render json: {
+        user: {
+          username: @user.username,
+          email: @user.email
+        }
+      }
+    else
+      render json: {
+        success: false
+      }
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
